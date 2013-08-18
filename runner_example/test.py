bd7fb251fa40b664cf0a1ff9209419a415c54e7d
David Rothlisberger <david@rothlis.net>
2013-08-18 13:58:45 +0100

    runner-example: Fix failing script (for demonstration of stb-tester/extra/runner)

--- runner_example/test.py
+++ runner_example/test.py
@@ -3,4 +3,5 @@ import stbt
 stbt.wait_for_match('videotestsrc-redblue.png', consecutive_matches=24)
 stbt.press('checkers-8')
 stbt.wait_for_match('videotestsrc-checkers-8.png', consecutive_matches=24)
+stbt.press('ball')
 stbt.wait_for_motion(consecutive_frames=24)
